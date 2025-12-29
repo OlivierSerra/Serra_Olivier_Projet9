f95a19eb6de428aba5b7c58247f8d7b435781100
OlivierSerra <o75serra@gmail.com>
2025-12-29 19:35:42 +0100

    final projet9

--- gateway/src/main/java/com/medilabo/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/medilabo/gateway/config/SecurityConfig.java
@@ -13,7 +13,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -22,54 +25,39 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
-                // désactivation car authentification HTTP, ss session,  peu d'intérêt d'augmenter la securité
-                // donc pas la peine d'avoir un loginform donc greenIT indique la légèreté
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
 
                 .authorizeExchange(exchanges -> exchanges
-                        // autorise les routes sans identifications
-                        .pathMatchers("/actuator/health", "/favicon.ico",
-                                "/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
-                        // autorise les routes au seul admin
-                        .pathMatchers("/patients/ui/**", "/notes/ui/**", "/risk/ui/**").permitAll()
-                        // autorise les routes au user
-                        .pathMatchers("/actuator/**").hasRole("ADMIN")
-                        .pathMatchers("/patients/**", "/notes/**", "/risk/**").hasRole("USER")
-
+                        .pathMatchers("/login", "/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                // activation de l'authentification HTTP Basic -1er n iveau d'authentification ->
-                // a chq requete, vérification du password
-                .httpBasic(Customizer.withDefaults())
 
-                //absence de session HTTP ou Oauth
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .formLogin(form -> form.loginPage("/login"))
 
                 .logout(logout -> logout
-                        .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.POST, "/logout"))
-                        .logoutSuccessHandler((webFilterExchange, authentication) -> {
-                            var response = webFilterExchange.getExchange().getResponse();
-                            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-                            response.getHeaders().set(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"Realm\"");
-                            return response.setComplete();
-                        })
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler(
+                                new RedirectServerLogoutSuccessHandler() {{
+                                    setLogoutSuccessUrl(URI.create("/login"));
+                                }}
+                        )
                 )
 
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .build();
     }
 
     @Bean
-    //porte le nom de l'import MapReactiveUserDetailsService
-    //userDetailsService adapté auwebFlux. = c'est le InMemoryUserDetailsManager en spring MCV
     public MapReactiveUserDetailsService users(PasswordEncoder encoder) {
         return new MapReactiveUserDetailsService(
-                User.withUsername("user").password(encoder.encode("root")).roles("USER").build(),
-                User.withUsername("admin").password(encoder.encode("root")).roles("ADMIN").build()
+                User.withUsername("user")
+                        .password(encoder.encode("root"))
+                        .roles("USER")   // rôle “symbolique”, pas de gestion de droits
+                        .build()
         );
     }
 
     @Bean
-    //encodage du mot de passe
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
--- gateway/src/main/java/com/medilabo/gateway/controller/LoginController.java
+++ gateway/src/main/java/com/medilabo/gateway/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/login")
     public String login() {
+
         return "login";
     }
 }
